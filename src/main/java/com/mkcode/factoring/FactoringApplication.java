2a3ec5d4bc50da457786e5c3e892efd148dd1e73
mink1325 <mindaugas.kairys@gmail.com>
2020-09-08 23:07:33 +0300

    initial spring boot generated app

---
+++ src/main/java/com/mkcode/factoring/FactoringApplication.java
@@ -0,0 +1,13 @@
+package com.mkcode.factoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkcode/factoring/FactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkcode.factoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
